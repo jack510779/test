54654dcac82ce8abf8abd573a3fd7b91e2898d5d
jack510779 <jack510779@gmail.com>
2024-10-05 19:58:20 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+const data = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [22.98152659492633,120.22505780791008],
+      },
+      properties: {
+       測試中
+      },
+    },
+  ],
+};
